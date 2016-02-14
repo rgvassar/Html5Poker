9eef8eecb1ce15f2f5aa2bbe522b7f8d2844b114
Russell Vassar <rvassar@russellvassar.com>
2016-02-13 17:19:19 -0800

    Merged client and server package.json dependencies to get this working on heroku.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var tableRouter = require("./routes/table-router");
 var accountRouter = require("./routes/account-router");
 var app = express();
 app.use("/", express.static(__dirname + "/wwwroot"));
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
 app.get("/tables", tableRouter.tables);
 app.get("/account", accountRouter.account);
 app.listen(process.env.PORT || 3000);
--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import accountRouter = require("./routes/account-router");
 let app: express.Express = express();
 
 app.use("/", express.static(__dirname + "/wwwroot" ));
+app.use("/node_modules", express.static(__dirname + "/node_modules" ));
 app.get("/tables", tableRouter.tables);
 app.get("/account", accountRouter.account);
 app.listen(process.env.PORT || 3000);
