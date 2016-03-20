72be9c7f785aceead421a802306ba6fc18c2dac1
Russell Vassar <rvassar@russellvassar.com>
2016-03-19 21:03:09 -0700

    Don't need linting from gulp anymore. Using atom plugin.

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,21 +1,12 @@
 import * as gulp from "gulp";
-import tslint from "gulp-tslint";
 import * as sass from "gulp-sass";
 import * as autoprefixer from "gulp-autoprefixer";
 import * as ts from "gulp-typescript";
 import * as sourcemaps from "gulp-sourcemaps";
 
-const stylish = require("gulp-tslint-stylish");
 const sassFiles = "./wwwroot/app/**/*.scss";
 const tsProject = ts.createProject("tsconfig.json");
 const tsFiles = ["./*.ts", "./**/*.ts"];
-
-gulp.task("vet", () => {
-    return gulp
-        .src(tsFiles)
-        .pipe(tslint())
-        .pipe(tslint.report(stylish));
-});
 
 gulp.task("styles", () => {
     return gulp
