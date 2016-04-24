ffdbfd65e0e5dcf6edc0644ff210ba46c4b91f92
Russell Vassar <rvassar@russellvassar.com>
2016-04-24 15:48:05 -0700

    Added validation and redirect to registration page.

--- wwwroot/app/components/app/app.component.ts
+++ wwwroot/app/components/app/app.component.ts
@@ -1,5 +1,6 @@
 /// <reference path="../../../../node_modules/angular2/typings/browser.d.ts"/>
 import {Component, ViewChild} from "angular2/core";
+import {FORM_PROVIDERS} from "angular2/common";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig} from "angular2/router";
 import {TableService} from "../../services/table/table.service";
@@ -9,7 +10,7 @@ import {RegisterComponent} from "../register/register.component";
 
 @Component({
     directives: [PlayComponent, RegisterComponent, ROUTER_DIRECTIVES],
-    providers: [TableService, AccountService, HTTP_PROVIDERS, ROUTER_PROVIDERS],
+    providers: [TableService, AccountService, HTTP_PROVIDERS, ROUTER_PROVIDERS, FORM_PROVIDERS],
     selector: "my-app",
     styleUrls: ["app/components/app/app.css"],
     templateUrl: "app/components/app/app.html"
--- wwwroot/app/components/register/register.component.ts
+++ wwwroot/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "angular2/core";
+import {Router} from 'angular2/router';
+import {FormBuilder, Validators, ControlGroup, Control} from "angular2/common";
 import {AccountService} from "../../services/account/account.service";
 
 @Component({
@@ -9,17 +11,34 @@ import {AccountService} from "../../services/account/account.service";
 
 export class RegisterComponent {
     public email: string;
-    public password: string;
     public passwordConf: string;
+    public passwordsMatch: boolean = true;
     public displayName: string;
+    public regForm: ControlGroup;
+    public password: Control = new Control("", Validators.compose([
+        Validators.minLength(8),
+        Validators.maxLength(160),
+        Validators.pattern("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,160}")
+    ]));
 
-    constructor(private _accountService: AccountService) {
+    constructor(private _accountService: AccountService, private _formBuilder: FormBuilder, private _router: Router) {
+        this.regForm = this._formBuilder.group({
+            "password": this.password,
+            "email": this.email
+        });
 
     }
 
     public onSubmit() {
-        this._accountService.addAccount(this.email, this.displayName, this.password).subscribe(
-            () => console.log("done poosting!")
-        );
+        if (this.passwordConf !== this.password.value) {
+            this.passwordsMatch = false;
+        } else {
+            this.passwordsMatch = true;
+        }
+        if (this.passwordsMatch) {
+            this._accountService.addAccount(this.email, this.displayName, this.password.value).subscribe(
+                data => this._router.navigate(["PlayPoker"])
+            );
+        }
     }
 }
